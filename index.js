3cc8221390260e05f91172eacc0dad46482a49df
Kurtis Garcia <kurtisgarcia14@gmail.com>
2021-09-16 14:15:52 -0400

    fixed media query for ipad

--- index.js
+++ index.js
@@ -5,6 +5,3 @@ const scroll = new SmoothScroll('a[href*="#"]', {
     speedAsDuration: true,
     header: '[data-scroll-header]',
   });
-
-
-  // $enable-smooth-scroll	true (default) or false	Applies scroll-behavior: smooth globally, except for users asking for reduced motion through prefers-reduced-motion media query
