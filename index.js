b6cc41de49d37b4a9fd3bc50676bebd277149a49
Kurtis Garcia <kurtisgarcia14@gmail.com>
2021-10-06 11:30:52 -0400

    testing

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ gsap.from('.web-dev', {
   },
   y: 200,
   opacity: 0,
-  duration: 1.5,
+  duration: 1,
 })
 gsap.from('.res-des', {
   scrollTrigger: {
@@ -23,7 +23,7 @@ gsap.from('.res-des', {
   },
   y: 200,
   opacity: 0,
-  duration: 1.5,
+  duration: 1,
 })
 gsap.from('.customer-service', {
   scrollTrigger: {
@@ -31,16 +31,113 @@ gsap.from('.customer-service', {
   },
   y: 200,
   opacity: 0,
-  duration: 1.5,
+  duration: 1,
 })
 
-// let tl = gsap.timeline({
-//   scrollTrigger: {
-//     trigger: '.skills-container',
-//     scrub: true,
-//   }
-// });
+gsap.from('.icon-group1-animate', {
+  scrollTrigger: {
+    trigger: '.icon-group1-animate',
+  },
+  y: 200,
+  opacity: 0,
+  duration: 1,
+})
+gsap.from('.icon-group2-animate', {
+  scrollTrigger: {
+    trigger: '.icon-group2-animate',
+  },
+  y: 200,
+  opacity: 0,
+  duration: 1,
+})
 
-// tl.from('.web-dev', {y: 100, opacity: 0, duration: 1.5})
-//   .from('.res-des', {y: 100, opacity: 0, duration: 1.5}, "-=1.5")
-//   .from('.customer-service', {y: 100, opacity: 0, duration: 1.5}, "-=1.5")
+gsap.from('.project1-content-animate', {
+  scrollTrigger: {
+    trigger: '.project1-content-animate',
+    start: 'center bottom',
+  },
+  x: 150,
+  opacity: 0,
+  duration: 1.5,
+})
+gsap.from('.project1-thumb-animate', {
+  scrollTrigger: {
+    trigger: '.project1-thumb-animate',
+    start: 'center bottom',
+  },
+  x: 100,
+  opacity: 0,
+  duration: 1.5,
+})
+gsap.from('.project2-content-animate', {
+  scrollTrigger: {
+    trigger: '.project2-content-animate',
+    start: 'center bottom',
+  },
+  x: 150,
+  opacity: 0,
+  duration: 1.5,
+})
+gsap.from('.project2-thumb-animate', {
+  scrollTrigger: {
+    trigger: '.project2-thumb-animate',
+    start: 'center bottom',
+  },
+  x: 100,
+  opacity: 0,
+  duration: 1.5,
+})
+gsap.from('.project3-content-animate', {
+  scrollTrigger: {
+    trigger: '.project3-content-animate',
+    start: 'center bottom',
+  },
+  x: 150,
+  opacity: 0,
+  duration: 1.5,
+})
+gsap.from('.project3-thumb-animate', {
+  scrollTrigger: {
+    trigger: '.project3-thumb-animate',
+    start: 'center bottom',
+  },
+  x: 100,
+  opacity: 0,
+  duration: 1.5,
+})
+gsap.from('.project4-content-animate', {
+  scrollTrigger: {
+    trigger: '.project4-content-animate',
+    start: 'center bottom',
+  },
+  x: 150,
+  opacity: 0,
+  duration: 1.5,
+})
+gsap.from('.project4-thumb-animate', {
+  scrollTrigger: {
+    trigger: '.project4-thumb-animate',
+    start: 'center bottom',
+  },
+  x: 100,
+  opacity: 0,
+  duration: 1.5,
+})
+gsap.from('.project5-content-animate', {
+  scrollTrigger: {
+    trigger: '.project5-content-animate',
+    start: 'center bottom',
+  },
+  x: 150,
+  opacity: 0,
+  duration: 1.5,
+})
+gsap.from('.project5-thumb-animate', {
+  scrollTrigger: {
+    trigger: '.project5-thumb-animate',
+    start: 'center bottom',
+  },
+  x: 100,
+  opacity: 0,
+  duration: 1.5,
+})
