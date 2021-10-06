aa53e6d8d7525f0c51f9db1c9cde207b8cdfb255
Kurtis Garcia <kurtisgarcia14@gmail.com>
2021-10-06 00:41:01 -0400

    testing animations

--- index.js
+++ index.js
@@ -5,3 +5,45 @@ const scroll = new SmoothScroll('a[href*="#"]', {
     speedAsDuration: true,
     header: '[data-scroll-header]',
   });
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from('.web-dev', {
+  scrollTrigger: {
+    trigger: '.web-dev',
+    scrub: true,
+  },
+  y: 100,
+  opacity: 0,
+  duration: 1.5,
+  scrub: true,
+})
+gsap.from('.res-des', {
+  scrollTrigger: {
+    trigger: '.res-des',
+    scrub: true,
+  },
+  y: 100,
+  opacity: 0,
+  duration: 1.5,
+})
+gsap.from('.customer-service', {
+  scrollTrigger: {
+    trigger: '.customer-service',
+    scrub: true,
+  },
+  y: 100,
+  opacity: 0,
+  duration: 1.5,
+})
+
+// let tl = gsap.timeline({
+//   scrollTrigger: {
+//     trigger: '.skills-container',
+//     scrub: true,
+//   }
+// });
+
+// tl.from('.web-dev', {y: 100, opacity: 0, duration: 1.5})
+//   .from('.res-des', {y: 100, opacity: 0, duration: 1.5}, "-=1.5")
+//   .from('.customer-service', {y: 100, opacity: 0, duration: 1.5}, "-=1.5")
